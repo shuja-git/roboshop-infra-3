1cd689419bf13dff58584ef5a2a6a25f4cc751d8
shuja-git <shuja.devops@gmail.com>
2023-09-09 23:35:31 -0700

    ami replaced

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "devops-practice-with-ansible"
+  name_regex       = "instance-with-ansible"
   owners           = [data.aws_caller_identity.current.account_id]
 }
 resource "aws_instance" "ec2" {
