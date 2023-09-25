45a7d78eb6589bb9ccc52a4e86d79b0658abbeb4
shuja-git <shuja.devops@gmail.com>
2023-09-25 01:12:09 -0700

    quotes removed

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ module "ec2" {
   component = each.value["name"]
   instance_type = each.value["type"]
   env = var.env
-  monitor = try(each.value["monitor"], "false")
+  monitor = try(each.value["monitor"], false)
 }
 
